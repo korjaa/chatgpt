f4d05663769377747a5d49594da2d1431f2814ab
Jaakko Korhonen <jaakko.korh@gmail.com>
2023-05-19 07:19:59 +0300

    Make installable.

---
+++ setup.py
@@ -0,0 +1,25 @@
+import os
+from setuptools import setup, find_packages
+
+pkg_name = "chatgpt"
+
+# Get version
+exec(open(os.path.join(pkg_name, "version.py")).read())
+
+setup(
+    name=pkg_name,
+    version=__version__,
+    url=f"https://github.com/korjaa/{pkg_name}",
+    author="Jaakko Korhonen",
+    author_email="user@email.com",
+    description="ChatGPT commandline chat",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            f"{pkg_name}={pkg_name}.cli:main"
+        ]
+    },
+    install_requires=[
+        "openai==0.27.6"
+    ],
+)
