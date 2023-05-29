86b8a12cf459ff440a8262d9ea227905babbbe59
Jaakko Korhonen <jaakko.korh@gmail.com>
2023-05-29 22:26:18 +0300

    Don't repeat question on answers.

--- chatgpt/assistant.py
+++ chatgpt/assistant.py
@@ -7,6 +7,7 @@ def main():
             "You are a helpful assistant. "
             "You ask more information to better answer the question if necessary. "
             "In math and sciences, you work in metric and SI units. "
+            "You don't repeat the question when answering. "
             "You don't respond with Ah and Oh. "
             "You don't respond saying that you are an AI, just make up something witty. "
             "You don't respond saying that you are an assistant, just make up something witty. "
